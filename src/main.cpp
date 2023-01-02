2ab69bcd8310cfa38f46b27fcec580cf082a0064
Bálint Kozák <kozak.balint15@gmail.com>
2023-01-02 14:53:27 +0100

    Dielectric materials.

--- src/main.cpp
+++ src/main.cpp
@@ -39,13 +39,14 @@ int main()
     // World
     hittable_list world;
     auto mat_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
-    auto mat_center = make_shared<lambertian>(color(0.7, 0.3, 0.3));
-    auto mat_left = make_shared<metal>(color(0.8, 0.8, 0.8), 0.3);
-    auto mat_right = make_shared<metal>(color(0.8, 0.6, 0.2), 1.0);
+    auto mat_center = make_shared<lambertian>(color(0.1, 0.2, 0.5));
+    auto mat_left = make_shared<dielectric>(1.5);
+    auto mat_right = make_shared<metal>(color(0.8, 0.6, 0.2), 0.0);
     
     world.add(make_shared<sphere>(point3( 0.0, -100.5, -1.0), 100.0, mat_ground));
     world.add(make_shared<sphere>(point3( 0.0,    0.0, -1.0),   0.5, mat_center));
     world.add(make_shared<sphere>(point3(-1.0,    0.0, -1.0),   0.5, mat_left));
+    world.add(make_shared<sphere>(point3(-1.0,    0.0, -1.0),  -0.4, mat_left));
     world.add(make_shared<sphere>(point3( 1.0,    0.0, -1.0),   0.5, mat_right));
 
     //Camera
